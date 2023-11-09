00ab5cda4a3ee07f87e38def00087a1237bc84e1
klegum <148566702+klegum@users.noreply.github.com>
2023-11-09 18:27:54 +0000

    coded a way to calculate the determinant

---
+++ stabilityAnalysis.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cplot
+
+ah = 0.2
+bh = 0.4
+k = -0.2912416558
+a = 1
+b1 = 0
+b2 = 0
+cccDelay = 0.5
+humanDelay = 1
+
+L = np.matrix([[0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0],
+              [-1, 1, 0, 0, 0, 0],
+              [0, -1, 1, 0, 0, 0],
+              [1, 0, -1, 0, 0, 0]])
+P = np.matrix([[0, 0, 0, 0, 0, 0],
+              [0, -(ah + bh), bh, 0, ah * k, 0],
+              [bh, 0, -(ah + bh), 0, 0, ah * k],
+              [0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0]])
+R = np.matrix([[-(a + b1 + b2), b1, b2, a * k, 0, 0],
+              [0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0]])
+I = np.matrix([[1, 0, 0, 0, 0, 0],
+              [0, 1, 0, 0, 0, 0],
+              [0, 0, 1, 0, 0, 0],
+              [0, 0, 0, 1, 0, 0],
+              [0, 0, 0, 0, 1, 0],
+              [0, 0, 0, 0, 0, 1]])
+
+def f(gamma):
+    return float(np.linalg.det((gamma * I) - L - (P * (np.exp(1) ** (-gamma * humanDelay))) - (R * (np.exp(1) ** (-gamma * cccDelay)))))
+
+print([f(gamma*1j) for gamma in range(100)])
