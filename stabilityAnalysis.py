685c0c155b78b9b5e4f66e9458e58b1d576d2f2f
Ojas Sharma <23020583@iclms.ac.uk>
2023-11-16 21:09:50 +0000

    possibly added checking for a sign change?

--- stabilityAnalysis.py
+++ stabilityAnalysis.py
@@ -10,6 +10,9 @@ b2 = 0
 cccDelay = 0.5
 humanDelay = 1
 
+def f(gamma):
+    return np.linalg.det((gamma * I) - L - (P * (np.exp(1) ** (-gamma * humanDelay))) - (R * (np.exp(1) ** (-gamma * cccDelay))))
+
 L = np.matrix([[0, 0, 0, 0, 0, 0], 
               [0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0],
@@ -55,20 +58,45 @@ def makeMesh(square):
     quartersquare3[:, 1] += (sideLength/2)
     quartersquare4 = quartersquare.copy()
     quartersquare4 += (sideLength/2)
-    return np.concatenate((quartersquare, quartersquare2, quartersquare3, quartersquare4), axis=0)
+    return np.array([quartersquare3, quartersquare4, quartersquare, quartersquare2])
 
 sideLength = (max - min) / N-1
 wholeSquare = unitSquare*max*2
 firstQuarter = quarterSquare(wholeSquare)
 initialMesh = makeMesh(wholeSquare)
 secondMesh = makeMesh
-plt.plot(initialMesh[:, 0], initialMesh[:, 1], 'go')
+#print(initialMesh)
+#plt.plot(initialMesh[:, 0], initialMesh[:, 1], 'go')
 plt.show()
 nodes = []
 
-#print(nodes)
+for square in initialMesh:
+    realChanged = False
+    imaginaryChanged = False
+    reals = []
+    imags = []
 
-def f(gamma):
-    return np.linalg.det((gamma * I) - L - (P * (np.exp(1) ** (-gamma * humanDelay))) - (R * (np.exp(1) ** (-gamma * cccDelay))))
+    for node in square:
+        value = f(node[0] + node[1]*1j)
+        #print(value)
+        reals.append(value.real)
+        imags.append(value.imag)
+    #print(reals)
+    #print(imags)
+
+    if not(all(i >= 0 for i in reals) or all(i < 0 for i in reals)):
+        realChanged = True
+
+    if not(all(i >= 0 for i in imags) or all(i < 0 for i in imags)):
+        imaginaryChanged = True
+
+    if (imaginaryChanged and realChanged):
+        print("CHANGED!") # further split square
+    else:
+        print(square) # omit square
+        np.delete(initialMesh, square)
+        
+    
+#print(nodes)
 
 #print([f(gamma*1j) for gamma in range(100)])
